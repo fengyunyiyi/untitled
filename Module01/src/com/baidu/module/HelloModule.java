1a5a29643707fa99f34f08708d746d7e7c859b9f
MyMac <MyMac@MyMac.com>
2020-11-16 10:23:32 +0800

    999

---
+++ Module01/src/com/baidu/module/HelloModule.java
@@ -0,0 +1,24 @@
+package com.baidu.module;
+
+import java.util.ArrayList;
+
+/**
+ * Created by MyMac on 2020/11/15.
+ */
+public class HelloModule {
+    public static void main(String arg[]) {
+        System.out.println("HelloWorld Module!");
+    }
+
+    public void m() {
+
+    }
+
+    public void a() {
+
+    }
+
+    ArrayList arrayList = new ArrayList();
+
+
+}
