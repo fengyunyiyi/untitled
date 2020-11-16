1771220598c4580d7911dd7b87b2658f565023c2
MyMac <MyMac@MyMac.com>
2020-11-16 10:39:56 +0800

    999

---
+++ src/com/baidu/chat/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.baidu.chat;
+
+/**
+ * Created by MyMac on 2020/11/15.
+ */
+public class HelloWorld {
+    public static  void  main(String arg[]){
+        System.out.println("HelloWorld!");
+    }
+}
